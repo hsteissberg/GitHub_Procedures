3e2eb1eda7122fdae4d498e022d51b76647ed254
Todd Steissberg <todd.steissberg@gmail.com>
2021-06-06 23:51:35 -0700

    Added assignee field, and several bug fixes

--- download_github_issues_from_repo_requests.py
+++ download_github_issues_from_repo_requests.py
@@ -17,52 +17,59 @@ from pprint import pprint
 
 
 # Get data from GitHub
-def get_request(user_name, repo_name, token, state='open'):
+def get_request(user_name, token, repo_name, params={}):
     query_url = f"https://api.github.com/repos/{user_name}/{repo_name}/issues?per_page=200"
-    params = { 'state': state }
     headers = {'Authorization': f'token {token}'}
     issues = requests.get(query_url, headers=headers, params=params)
     return issues.json()
 
 # Export results to CSV
-def export_issues(outfile, user_name, repositories, token, state='open'):
+def export_issues(outfile, user_name, token, repositories, params={}, issue_labels=[]):
     print(f'Processing: {repo_name}')
-    issues = get_request(user_name, repo_name, token, state=state)
-    print(f'Length of issues = {len(issues)}')
+    issues = get_request(user_name, token, repo_name, params=params)
+    print(f'Number of issues: {len(issues)}')
     g = open(outfile, 'w')
-    g.write('Title,Labels\n')
+    g.write('Title,Assignee,Labels\n')
+    # g.write('Title,Labels\n')
     for issue in issues:
         try:
             title = issue['title']
+            print(title)
             labels = issue['labels']
-            hline = f'"{title}","'
-            for label in labels:
-                hline += label['name'] + ', '
-            hline = hline.strip().strip(',') + '"\n'
-            g.write(hline)
-        except:
-            print(issue)
-    g.close
-    # pprint(r.json())
+            assignees = issue['assignees']
 
-# Export results to CSV, only if the specified label is in the issue
-def export_issues_with_label(outfile, user_name, repositories, token, state='open', issue_label='bug'):
-    print(f'Processing: {repo_name}')
-    issues = get_request(user_name, repo_name, token, state='open')
-    print(f'Length of issues = {len(issues)}')
-    g = open(outfile, 'w')
-    g.write('Title,Labels\n')
-    for issue in issues:
-        try:
-            title = issue['title']
-            labels = issue['labels']
-            for label in labels:
-                label_name = label['name']
-                if label_name == issue_label:
-                    g.write(f'"{title}","{label_name}"\n')
+            hline = f'"{title}",'
+
+            if len(assignees) > 0:
+                for assignee in assignees:
+                    hline += f'''"{assignee['login']}", '''
+                hline = hline.strip() # Save the trailing comma for creating the labels column
+            else:
+                hline += '"Not assigned",'
+            
+            # Optionally skip writing of the issue if the specified label is not found
+            write_data = True
+            if len(issue_labels) > 0:
+                write_data = False
+
+            # Iterate through labels and optionally check if a specified label is present
+            if len(labels) > 0:
+                for label in labels:
+                    label_name = label['name']
+                    if len(issue_labels) > 0:
+                        if label_name in issue_labels:
+                            write_data = True
+                    hline += f'"{label_name}", '
+                hline = hline.strip().strip(',') + '\n'
+            else:
+                hline += '"No labels"\n'
+
+            if write_data:
+                g.write(hline)
         except:
             print(issue)
     g.close
+
 
 if __name__ == '__main__':
     user_name = 'EnvironmentalSystems'
@@ -97,7 +104,12 @@ if __name__ == '__main__':
         'WQ-Prototypes-and-Scripts'
     ]
 
-    state = 'open'
+    repositories = ['ScreamingPlants']
+
     for repo_name in repositories:
-        export_issues(f'output/{repo_name}_issues.csv', user_name, repositories, token, state=state)
-        export_issues_with_label(f'output/{repo_name}_issues_with_bugs.csv', user_name, repositories, token, state=state, issue_label='effort: 8')
+        params = { 'state': 'open'}
+        export_issues(f'output/{repo_name}_open_issues.csv', user_name, token, repositories, params=params)
+        params = { 'state': 'closed'}
+        export_issues(f'output/{repo_name}_closed_issues.csv', user_name, token, repositories, params=params)
+        params = { 'state': 'open'}
+        export_issues(f'output/{repo_name}_issues_with_bugs.csv', user_name, token, repositories, params=params, issue_labels=['bug', 'effort: 8'])
